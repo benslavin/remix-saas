8fedb4a7a69f6fc26e20addcf85a6e16a3f886cf
Ben Slavin <ben@benslavin.com>
2025-03-11 15:12:23 -0400

    chore: implementing v3_routeConfig with remix-flat-routes

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import { vitePlugin as remix } from '@remix-run/dev'
 import { defineConfig } from 'vite'
-import { flatRoutes } from 'remix-flat-routes'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
 declare module '@remix-run/node' {
@@ -14,15 +13,13 @@ export default defineConfig({
     remix({
       serverModuleFormat: 'esm',
       ignoredRouteFiles: ['**/.*'],
-      routes: async (defineRoutes) => {
-        return flatRoutes('routes', defineRoutes)
-      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
         v3_singleFetch: true,
         v3_lazyRouteDiscovery: true,
+        v3_routeConfig: true,
       },
     }),
     tsconfigPaths(),
